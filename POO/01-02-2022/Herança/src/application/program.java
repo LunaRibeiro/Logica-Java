31d8e1006feec09f7916091311fa3057a93faade
LunaRibeiro <lunaribeiroalves28@gmail.com>
2022-02-02 00:18:39 -0300

    Herança

---
+++ POO/01-02-2022/Herança/src/application/program.java
@@ -0,0 +1,25 @@
+package application;
+
+import entities.cachorro;
+import entities.cavalo;
+import entities.preguica;
+
+public class program {
+	public static void main(String[] args) {
+		
+		cachorro Tobs = new cachorro("Tobs", 6, true, "Nenem", true);
+		
+		cavalo Ventania = new cavalo("Ventania", 4, true, true);
+		
+		preguica Guita = new preguica("Pépe", 3, false, true);
+		
+		System.out.println(Tobs);
+		System.out.println();
+		
+		System.out.println(Ventania);
+		System.out.println();
+		
+		System.out.println(Guita);
+	}
+
+}
